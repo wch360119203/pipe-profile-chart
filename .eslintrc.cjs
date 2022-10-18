3ef963badd0b8a12e178270b63f0864dbd38880c
wuch <360119203@qq.com>
2022-10-18 17:20:26 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,10 @@ module.exports = {
     'vue/no-multiple-template-root': 'off',
     'no-plusplus': 'off',
     'prettier/prettier': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'no-unused-vars': 1,
   },
   globals: {
     NodeJS: true,
